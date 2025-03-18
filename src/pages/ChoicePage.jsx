69655bae59ee8f930e0dd05eb38058a3b406ff68
[Gaye Dinç] <gayedinc190@gmail.com>
2025-03-18 23:49:04 +0300

    choice-page css düzenleniyor

--- src/pages/ChoicePage.jsx
+++ src/pages/ChoicePage.jsx
@@ -1,6 +1,7 @@
-import { useState, useContext, useEffect } from "react";
-import { Link, usePage } from '../Router'
+import { useContext } from "react";
+import { Link } from '../Router'
 import { GameContext } from "./GameContext";
+import { CrossSvg, CircleSvg } from "../Svg";
 
 export default function ChoicePage() {
   const { playerMark, setPlayerMark } = useContext(GameContext);
@@ -12,19 +13,22 @@ export default function ChoicePage() {
   };
 
   return (
-    <div className="choice-content">
-      <h1>PICK PLAYER 1’S MARK</h1>
-      <div className="choice-buttons">
-        <button
-          className={`choice-button ${playerMark === "X" ? "active" : ""}`}
-          onClick={() => handlePlayerChoice("X")}>X</button>
-        <button
-          className={`choice-button ${playerMark === "O" ? "active" : ""}`}
-          onClick={() => handlePlayerChoice("O")}>O</button>
+    <div className="choice-page">
+      <div className="choice-content">
+        <h1>PICK PLAYER 1’S MARK</h1>
+        <div className="choice-area">
+          <button
+            className={`choice-button ${playerMark === "X" ? "active" : ""}`}
+            onClick={() => handlePlayerChoice("X")}><CrossSvg /></button>
+          <button
+            className={`choice-button ${playerMark === "O" ? "active" : ""}`}
+            onClick={() => handlePlayerChoice("O")}><CircleSvg /></button>
+        </div>
+        <p>REMEMBER : X GOES FIRST</p>
       </div>
-      <p>REMEMBER : X GOES FIRST</p>
-      <p>Selected: {playerMark}</p>
-      <button><Link href="/game">NEW GAME (VS CPU)</Link></button>
+      <div className="choice-btns">
+        <button className="new-game-cpu"><Link href="/game">NEW GAME (VS CPU)</Link></button>
+      </div>
     </div>
   );
 }
