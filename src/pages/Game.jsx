390e515239812fffaf914c3d881ce792608d43a0
omer-kuluc <omerkuluc96@gmail.com>
2025-03-27 22:28:09 +0300

    cpu hamlelerindeki sorun çözüldü

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from "react";
-import { Link } from '../Router'
+import { Link } from "../Router";
 import "../assets/Game.css";
 import "../App.css";
 import { CrossSvg, CircleSvg, RestartSvg } from "../Svg";
@@ -11,46 +11,32 @@ export default function Game() {
 
   const [boxes, setBoxes] = useState(Array(9).fill(null));
   const [emptyBoxes, setEmptyBoxes] = useState([...Array(9).keys()]);
-  const [isUserTurn, setIsUserTurn] = useState(true); // sıranın kullanıcıda olma durumu
+  const [isUserTurn, setIsUserTurn] = useState(true);
   const [userChoices, setUserChoices] = useState([]);
   const [cpuChoices, setCpuChoices] = useState([]);
-  const [gameOver, setGameOver] = useState(false); // Oyunun bittiğini takip eden state
+  const [gameOver, setGameOver] = useState(false);
   const [winner, setWinner] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [ties, setTies] = useState(0);
 
   const winnerCombs = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
   ];
 
   const [selectedMark, setSelectedMark] = useState(null);
 
   useEffect(() => {
-    const storedMark = localStorage.playerMark; // kullanıcının seçtiği işareti al
-    const storedCpuMark = localStorage.cpuMark; // cpunun kullanacağı işareti al
-    if (storedMark) { // eğer kullanıcı bir işaret seçtiyse
+    const storedMark = localStorage.getItem("playerMark");
+    const storedCpuMark = localStorage.getItem("cpuMark");
+    if (storedMark) {
       setSelectedMark(storedMark);
-      setPlayerMark(storedMark); // kullanıcının seçtiği işareti güncelle
-      setCpuMark(storedCpuMark); // cpunun kullanacağı işareti güncelle
-
-      // kullanıcı o seçtiyse cpu başlar, x seçtiyse kullanıcı başlar
+      setPlayerMark(storedMark);
+      setCpuMark(storedCpuMark);
       setIsUserTurn(storedMark === "X");
     }
   }, [setPlayerMark, setCpuMark]);
-
-  useEffect(() => {
-    // Eğer cpunun başlaması gerekiyorsa ve oyun bitmediyse ilk hamleyi cpu yapsın
-    if (!isUserTurn && !gameOver) {
-      setTimeout(() => handleCPUMove(emptyBoxes, boxes), 750);
-    }
-  }, [isUserTurn]);
 
   function resetGame() {
     setTimeout(() => {
@@ -59,7 +45,7 @@ export default function Game() {
       setUserChoices([]);
       setCpuChoices([]);
       setIsUserTurn(selectedMark === "X");
-      setGameOver(false); // oyunun bittiğini sıfırla
+      setGameOver(false);
       setShowModal(false);
     }, 300);
   }
@@ -71,7 +57,7 @@ export default function Game() {
   }
 
   function handleBox(index) {
-    if (!isUserTurn || boxes[index] || gameOver) return; // Oyun bittiyse veya kutu doluysa işlem yapma
+    if (!isUserTurn || boxes[index] || gameOver) return;
 
     const newBoxes = [...boxes];
     newBoxes[index] = playerMark === "X" ? <CrossSvg /> : <CircleSvg />;
@@ -81,19 +67,18 @@ export default function Game() {
 
     setBoxes(newBoxes);
     setEmptyBoxes(updatedEmptyBoxes);
-    console.log(updatedEmptyBoxes);
     setUserChoices(newUserChoices);
 
     if (checkWinner(newUserChoices)) {
-      setGameOver(true); // Oyunu bitir
+      setGameOver(true);
       setTimeout(() => {
-        setWinner("YOU"); // Kazananı belirle
-        setShowModal(true); // Modalı aç
+        setWinner("YOU");
+        setShowModal(true);
       }, 200);
       return;
     }
 
-    if (updatedEmptyBoxes.length === 0 && !checkWinner(newUserChoices)) {
+    if (updatedEmptyBoxes.length === 0) {
       setGameOver(true);
       setTies((prev) => prev + 1);
       setTimeout(() => {
@@ -104,29 +89,28 @@ export default function Game() {
     }
 
     setIsUserTurn(false);
-
     setTimeout(() => {
       handleCPUMove(updatedEmptyBoxes, newBoxes);
     }, 750);
   }
 
   function handleCPUMove(updatedEmptyBoxes, updateBoxes) {
-    if (updatedEmptyBoxes.length === 0 || gameOver) return; // Oyun bittiyse işlem yapma
+    if (updatedEmptyBoxes.length === 0 || gameOver) return;
 
-    const x = Math.floor(Math.random() * updatedEmptyBoxes.length);
-    const cpuIndex = updatedEmptyBoxes[x];
-
-    const newCpuChoices = [...cpuChoices, cpuIndex];
-    setCpuChoices(newCpuChoices);
+    const cpuIndex = updatedEmptyBoxes[Math.floor(Math.random() * updatedEmptyBoxes.length)];
 
     const newBoxes = [...updateBoxes];
     newBoxes[cpuIndex] = playerMark === "X" ? <CircleSvg /> : <CrossSvg />;
 
     const remainingEmptyBoxes = updatedEmptyBoxes.filter((i) => i !== cpuIndex);
+
+    setCpuChoices((prevCpuChoices) => [...prevCpuChoices, cpuIndex]);
     setBoxes(newBoxes);
     setEmptyBoxes(remainingEmptyBoxes);
+  }
 
-    if (checkWinner(newCpuChoices)) {
+  useEffect(() => {
+    if (cpuChoices.length > 0 && checkWinner(cpuChoices)) {
       setGameOver(true);
       setTimeout(() => {
         setWinner("CPU");
@@ -135,18 +119,17 @@ export default function Game() {
       return;
     }
 
-    if (updatedEmptyBoxes.length === 0 && !checkWinner(newCpuChoices)) {
+    if (emptyBoxes.length === 0) {
       setGameOver(true);
       setTies((prev) => prev + 1);
       setTimeout(() => {
         setWinner("TIE");
         setShowModal(true);
       }, 200);
-      return;
+    } else {
+      setIsUserTurn(true);
     }
-
-    setIsUserTurn(true);
-  }
+  }, [cpuChoices]);
 
   return (
     <>
@@ -164,19 +147,20 @@ export default function Game() {
           <RestartSvg />
         </div>
       </header>
+
       <div className="game-area">
         <div className="game-boxes-area">
           {boxes.map((box, i) => (
             <div
               key={i}
-              className={`box ${isUserTurn && !box ? (playerMark === "X" ? "hover-x" : "hover-o") : ""
-                }`}
+              className={`box ${isUserTurn && !box ? (playerMark === "X" ? "hover-x" : "hover-o") : ""}`}
               onClick={() => handleBox(i)}
             >
               {box}
             </div>
           ))}
         </div>
+
         <div className="score-area">
           <div className="player-score-section">
             <h3>{selectedMark} (YOU)</h3>
@@ -200,11 +184,7 @@ export default function Game() {
                   <h5>YOU WON!</h5>
                   <div className="takesTheRoundPlayer">
                     {playerMark === "X" ? <CrossSvg /> : <CircleSvg />}
-                    <h4
-                      style={{
-                        color: playerMark === "X" ? "#31C3BD" : "#F2B137",
-                      }}
-                    >
+                    <h4 style={{ color: playerMark === "X" ? "#31C3BD" : "#F2B137" }}>
                       TAKES THE ROUND
                     </h4>
                   </div>
@@ -214,12 +194,8 @@ export default function Game() {
                 <div className="winnerScreenText">
                   <h5>OH NO, YOU LOST…</h5>
                   <div className="takesTheRoundCpu">
-                    {playerMark === "X" ? <CircleSvg /> : <CrossSvg />}{" "}
-                    <h4
-                      style={{
-                        color: playerMark === "X" ? "#F2B137" : "#31C3BD",
-                      }}
-                    >
+                    {playerMark === "X" ? <CircleSvg /> : <CrossSvg />}
+                    <h4 style={{ color: playerMark === "X" ? "#F2B137" : "#31C3BD" }}>
                       TAKES THE ROUND
                     </h4>
                   </div>
@@ -227,10 +203,7 @@ export default function Game() {
               )}
               {winner === "TIE" && <div className="rounTiedScreen"><h5>ROUND TIED</h5></div>}
               <div className="modal-buttons">
-                <button
-                  onClick={() => (window.location.href = "/choice-page")}
-                  className="quit-btn"
-                >
+                <button onClick={() => (window.location.href = "/choice-page")} className="quit-btn">
                   QUIT
                 </button>
                 <button onClick={resetGame} className="next-round-btn">
